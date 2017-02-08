5b99f29b850d60c56da690adbfb4760a3b5ce816
taras-zak <taras.zakan@gmail.com>
2017-02-09 00:37:54 +0200

    create app, add home_page

--- lists/lists/urls.py
+++ lists/lists/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from todo_lists import views
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home')
 ]
