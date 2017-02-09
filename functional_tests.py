b90e9b6327875cc5f58c1c8f2ba573c3842ce80c
taras-zak <taras.zakan@gmail.com>
2017-02-10 00:25:25 +0200

    update func tests, refactor unit test of home page

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
+import time
 import unittest
-from selenium import webdriver
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,14 +17,43 @@ class NewVisitorTest(unittest.TestCase):
         #Go to the page
         self.browser.get('http://localhost:8000')
 
+        # Check page title
         self.assertIn("To-Do", self.browser.title)
-        self.fail('Finish the test!')
+
+        # Check header
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # User find inputbox
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEquals(
+            inputbox.get_attribute('placeholder'),
+            'Enter to-do item'
+        )
 
         # User create item
+        inputbox.send_keys('Build home')
 
         # User press enter
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates. User see results
+        table = self.browser.find_element_by_id('id_test_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Build home' for row in rows)
+        )
+
+        # User try add another item
+
+        self.fail('Finish the test!')
+
+
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main(warnings="ignore")
--- lists/todo_lists/tests.py
+++ lists/todo_lists/tests.py
@@ -6,15 +6,6 @@ from todo_lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve("/")
-        self.assertEquals(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        print(response)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
