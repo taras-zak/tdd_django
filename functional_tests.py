814314cd74fdcc5c42756488b61840eacb91cc8e
taras-zak <taras.zakan@gmail.com>
2017-02-17 00:00:37 +0200

    handle post request, update ft

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn( row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Go to the page
         self.browser.get('http://localhost:8000')
@@ -39,19 +44,20 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates. User see results
-        table = self.browser.find_element_by_id('id_test_table')
-        rows = table.find_element_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Build home' for row in rows)
-        )
+        self.check_for_row_in_list_table('1: Build home')
 
         # User try add another item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Plant tree')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # Page updates  again, and shows two items
+        self.check_for_row_in_list_table('1: Build home')
+        self.check_for_row_in_list_table('2: Plant tree')
+
 
         self.fail('Finish the test!')
-
-
-
-
 
 
 
