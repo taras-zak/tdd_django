c4602d143a8b7d4041d1fc7bb06e9ae1801ec666
taras-zak <taras.zakan@gmail.com>
2017-02-18 00:22:16 +0200

    update template for display all items

--- lists/todo_lists/tests.py
+++ lists/todo_lists/tests.py
@@ -5,18 +5,40 @@ from django.http import HttpRequest
 from todo_lists.views import home_page
 from todo_lists.models import Item
 
+
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_sove_a_POST_request(self):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': "A new list item"})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_post(self):
         response = self.client.post('/', data={'item_text': "A new list item"})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
+
 
 class ItemModelTest(TestCase):
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
     def test_saving_and_retrieving_items(self):
         first_item = Item()
