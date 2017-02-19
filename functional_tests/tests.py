970d82214cbb7ae90b2b54626ea5d80f87b5d9c7
taras-zak <taras.zakan@gmail.com>
2017-02-20 00:32:25 +0200

    refactor FT: use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,26 +1,36 @@
 import time
-import unittest
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+MAX_WAIT = 10
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome('/data/work/231/chromedriver')
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn( row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Go to the page
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # Check page title
         self.assertIn("To-Do", self.browser.title)
@@ -41,25 +51,18 @@ class NewVisitorTest(unittest.TestCase):
 
         # User press enter
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates. User see results
-        self.check_for_row_in_list_table('1: Build home')
+        self.wait_for_row_in_list_table('1: Build home')
 
         # User try add another item
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Plant tree')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # Page updates  again, and shows two items
-        self.check_for_row_in_list_table('1: Build home')
-        self.check_for_row_in_list_table('2: Plant tree')
+        self.wait_for_row_in_list_table('1: Build home')
+        self.wait_for_row_in_list_table('2: Plant tree')
 
 
         self.fail('Finish the test!')
-
-
-
-if __name__ == "__main__":
-    unittest.main(warnings="ignore")
