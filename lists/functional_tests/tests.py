673f7b7ddf69dd0606a0e5e279e79a632008fd44
taras-zak <taras.zakan@gmail.com>
2017-03-05 19:10:46 +0200

    add deploy templates

--- lists/functional_tests/tests.py
+++ lists/functional_tests/tests.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.conf import settings
@@ -11,8 +12,17 @@ MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Chrome('/data/work/231/chromedriver')
 
     def tearDown(self):
         self.browser.quit()
@@ -32,7 +42,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_for_one_user(self):
         #Go to the page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # Check page title
         self.assertIn("To-Do", self.browser.title)
@@ -72,7 +82,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_multiple_users_can_start_lists_at_different_urls(self):
 
         # First user create todo list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy ticket')
         inputbox.send_keys(Keys.ENTER)
@@ -87,10 +97,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # First user gone
         self.browser.quit()
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Chrome('/data/work/231/chromedriver')
 
         # Second user come
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn("Buy honey", page_text)
         self.assertNotIn("make a trip", page_text)
@@ -114,7 +124,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # User goes to the homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # Notice that input is nicely centered
